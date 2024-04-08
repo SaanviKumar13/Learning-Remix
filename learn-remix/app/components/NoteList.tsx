7fe55232fa261b7bf0a5bfc1b2c7d7156cc16e9d
SaanviKumar13 <saanviprashantkumar@gmail.com>
2024-04-08 15:50:11 +0530

    fix: responsiveness

--- learn-remix/app/components/NoteList.tsx
+++ learn-remix/app/components/NoteList.tsx
@@ -10,20 +10,18 @@ export interface NoteType {
 
 export default function NoteList({ notes }: NoteType) {
   return (
-    <>
-      <div className="flex flex-row gap-5 flex-wrap items-center justify-center m-5">
-        {notes.map((note: Note, index: number) => (
-          <div
-            key={index}
-            className="bg-gray-100 h-44 overflow-y-scroll w-[30vw] p-5 rounded-lg m-2"
-          >
-            <h1 className="text-gray-800 text-center font-semibold text-xl">
-              {note.title}
-            </h1>
-            <p className="font-extralight text-gray-800">{note.content}</p>
-          </div>
-        ))}
-      </div>
-    </>
+    <div className="flex flex-row gap-5 flex-wrap items-center justify-center m-5">
+      {notes.map((note: Note, index: number) => (
+        <div
+          key={index}
+          className="bg-gray-100 h-44 overflow-y-scroll w-[80vw] md:w-[40vw] lg:w-[35vw] p-5 rounded-lg m-2"
+        >
+          <h1 className="text-gray-800 text-center font-semibold text-xl">
+            {note.title}
+          </h1>
+          <p className="font-extralight text-gray-800">{note.content}</p>
+        </div>
+      ))}
+    </div>
   );
 }
